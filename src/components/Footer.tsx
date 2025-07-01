f706e007000e4717e9db870f9724a4752b0a6674
Diego-Zica2 <diego.carvalho@maxmilhas.com.br>
2025-07-01 16:07:29 -0300

    carregando...

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -39,7 +39,7 @@ export function Footer() {
                           <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                           <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
                         </svg>
-                        <span className="ml-2 text-muted-foreground">Carregando imagem...</span>
+                        <span className="ml-2 text-muted-foreground">Carregando...</span>
                       </div>
                     )}
                     <img
